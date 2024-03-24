6570251c5dd7676b10d65620ee7e28f3aae7a042
Ryker Fish <rfish@mines.edu>
2024-03-23 18:29:57 -0600

    s1 c3 done

--- c3.py
+++ c3.py
@@ -1,14 +1,38 @@
- 	
-E 	11.1607% 	56.88 	M 	3.0129% 	15.36
-A 	8.4966% 	43.31 	H 	3.0034% 	15.31
-R 	7.5809% 	38.64 	G 	2.4705% 	12.59
-I 	7.5448% 	38.45 	B 	2.0720% 	10.56
-O 	7.1635% 	36.51 	F 	1.8121% 	9.24
-T 	6.9509% 	35.43 	Y 	1.7779% 	9.06
-N 	6.6544% 	33.92 	W 	1.2899% 	6.57
-S 	5.7351% 	29.23 	K 	1.1016% 	5.61
-L 	5.4893% 	27.98 	V 	1.0074% 	5.13
-C 	4.5388% 	23.13 	X 	0.2902% 	1.48
-U 	3.6308% 	18.51 	Z 	0.2722% 	1.39
-D 	3.3844% 	17.25 	J 	0.1965% 	1.00
-P 	3.1671% 	16.14 	Q 	0.1962% 	(1)
+import utilities as ut
+import numpy as np
+import scipy.stats as sp
+
+def rank(decode: str): # ranks based on the number of letters most common in english language
+    common: str = "etaoin shrdlu"
+    sum_common = 0
+
+    decode = decode.lower()
+
+    for c in common:
+        sum_common = sum_common + decode.count(c)
+
+    return sum_common
+
+freq = ut.getLetterFreq()
+
+hex_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+b_str = bytes.fromhex(hex_str)
+
+decode_freq = {}
+
+best_score = 0
+best_str = ""
+
+for key in range(0,128):
+
+    decode = bytes(b ^ key for b in b_str)
+    d_str = ut.b_to_ascii(decode).lower()
+
+    score = rank(d_str)
+
+    if score > best_score:
+        best_score = score
+        best_str = d_str
+
+print(best_str)
+print(best_score)
--- utilities.py
+++ utilities.py
@@ -5,9 +5,21 @@ def hexToNB64(h: str) -> str:
     b_b64: bytes = base64.b64encode(b_hex)
 
     return b_b64.decode("ascii")
+
+def b_to_ascii(b: bytes) -> str:
+    return b.decode("ascii")
     
 def xor(b1: bytes, b2: bytes) -> bytes:
     if(len(b1) != len(b2)):
         raise Exception("Arrays aren't same length")
 
     return  bytes(a ^ b for a,b in zip(b1, b2))
+
+def getLetterFreq():
+    freq_dict = {'e': 0.111607, 'm': 0.030129, 'a': 0.084966, 'h': 0.030034, 'r': 0.075809, 
+             'g': 0.024705, 'i': 0.075448, 'b': 0.02072, 'o': 0.071635, 'f': 0.018121, 
+             't': 0.069509, 'y': 0.017779, 'n': 0.066544, 'w': 0.012899, 's': 0.057351, 'k': 0.011016, 
+             'l': 0.054893, 'v': 0.010074, 'c': 0.045388, 'x': 0.002902, 'u': 0.036308, 
+             'z': 0.002721, 'd': 0.033844, 'j': 0.001965, 'p': 0.031671, 'q': 0.001962}
+    
+    return freq_dict
