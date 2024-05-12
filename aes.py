4cf85964054eda5af3734e9c32abd6865cf87298
Ryker Fish <rfish@mines.edu>
2024-05-12 17:34:53 -0600

    moved cbc functions

--- aes.py
+++ aes.py
@@ -1,4 +1,5 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import utilities as ut
 
 def ecb_encrypt(dat, key):
     cipher = Cipher(algorithms.AES(key), modes.ECB())
@@ -9,3 +10,44 @@ def ecb_decrypt(dat, key):
     cipher = Cipher(algorithms.AES(key), modes.ECB())
     decryptor = cipher.decryptor()
     return decryptor.update(dat) + decryptor.finalize()
+
+def cbc_encrypt(pt: bytearray, key: bytes, iv: bytes, blocklength: int = 16) -> bytearray:
+
+    pt = ut.pkcs_pad(pt)
+
+    n = len(pt)
+    n_blocks = int(n / blocklength)
+
+    ct = bytearray(n)
+    ct[0:blocklength] = ut.xor(pt[0:blocklength], iv)
+    ct[0:blocklength] = ecb_encrypt(ct[0:blocklength], key)
+
+    for i in range(1, n_blocks):
+        prev = (i-1)*blocklength
+        start = i*blocklength
+        end = start + blocklength
+
+        ct[start:end] = ut.xor(ct[prev:start], pt[start:end])
+        ct[start:end] = ecb_encrypt(ct[start:end], key)
+
+    return ct
+
+def cbc_decrypt(ct: bytearray, key: bytes, iv: bytes, blocklength: int = 16) -> bytearray:
+    n = len(ct)
+
+    n_blocks = int(n / blocklength)
+
+    pt = bytearray(n)
+    pt[0:blocklength] = ecb_decrypt(ct[0:blocklength], key)
+    pt[0:blocklength] = ut.xor(pt[0:blocklength], iv)
+
+
+    for i in range(1,n_blocks):
+        prev = (i-1)*blocklength
+        start = i*blocklength
+        end = start + blocklength
+
+        pt[start:end] = ecb_decrypt(ct[start:end], key)
+        pt[start:end] = ut.xor(ct[prev:start], pt[start:end])
+
+    return pt
--- c10.py
+++ c10.py
@@ -2,56 +2,12 @@ import utilities as ut
 import aes
 import base64
 
-def cbc_encrypt(pt: bytearray, key: bytes, iv: bytes, blocklength: int = 16) -> bytearray:
-
-    pt = ut.pkcs_pad(pt)
-
-    n = len(pt)
-    n_blocks = int(n / blocklength)
-
-    ct = bytearray(n)
-    ct[0:blocklength] = ut.xor(pt[0:blocklength], iv)
-    ct[0:blocklength] = aes.ecb_encrypt(ct[0:blocklength], key)
-
-    for i in range(1, n_blocks):
-        prev = (i-1)*blocklength
-        start = i*blocklength
-        end = start + blocklength
-
-        ct[start:end] = ut.xor(ct[prev:start], pt[start:end])
-        ct[start:end] = aes.ecb_encrypt(ct[start:end], key)
-
-    return ct
-
-
-def cbc_decrypt(ct: bytearray, key: bytes, iv: bytes, blocklength: int = 16) -> bytearray:
-    n = len(ct)
-
-    n_blocks = int(n / blocklength)
-
-    pt = bytearray(n)
-    pt[0:blocklength] = aes.ecb_decrypt(ct[0:blocklength], key)
-    pt[0:blocklength] = ut.xor(pt[0:blocklength], iv)
-
-
-    for i in range(1,n_blocks):
-        prev = (i-1)*blocklength
-        start = i*blocklength
-        end = start + blocklength
-
-        pt[start:end] = aes.ecb_decrypt(ct[start:end], key)
-        pt[start:end] = ut.xor(ct[prev:start], pt[start:end])
-
-    return pt
-
-
-
 
 test:bytearray = bytearray("testing testing!123 longer pls", encoding="ascii")
 key:bytes = bytes("poggers doggers!", encoding="ascii")
 iv = bytes(16)
-ct:bytearray = cbc_encrypt(test, key, iv)
-pt:bytearray = cbc_decrypt(ct, key, iv)
+ct:bytearray = aes.cbc_encrypt(test, key, iv)
+pt:bytearray = aes.cbc_decrypt(ct, key, iv)
 
 pt = ut.strip_pkcs(pt)
 assert(pt == test)
@@ -63,7 +19,8 @@ dat:bytearray = bytearray(base64.b64decode(b64))
 key = bytes("YELLOW SUBMARINE", encoding="ascii")
 iv = bytes(16) # all zeros
 
-pt = cbc_decrypt(dat, key, iv)
-# print(pt)
+pt = aes.cbc_decrypt(dat, key, iv)
+pt = ut.strip_pkcs(pt)
+print(pt)
 
 f.close()
